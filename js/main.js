efe45c565fedb088dd692f2583c05a776e4bd09e
rren22 <rren22@wisc.edu>
2026-03-07 00:09:05 -0600

    update proportional and slider

--- js/main.js
+++ js/main.js
@@ -6,92 +6,81 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; OpenStreetMap contributors'
 }).addTo(map);
 
+// month names
+const months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
+
 // load data
 fetch("data/CrimeIncidents2025Final.geojson")
 .then(response => response.json())
 .then(data => {
 
-    // process dates
-    var dates = processData(data);
-
     // create layer
     var geojsonLayer = L.geoJSON(data, {
         pointToLayer: function(feature, latlng) {
             return L.circleMarker(latlng, {
-                radius: 4,
+                radius: 0, // initial radius, updated by slider
                 fillOpacity: 0.8,
                 color: "black",
                 weight: 1
             });
         },
         onEachFeature: function(feature, layer) {
-            let popupContent = "";
-            for (let key in feature.properties) {
-                popupContent += "<strong>" + key + ":</strong> " +
-                                feature.properties[key] + "<br>";
-            }
+            let popupContent = "<strong>" + feature.properties.Neighborhood + "</strong><br>";
+            months.forEach(m => {
+                popupContent += m + ": " + feature.properties[m] + "<br>";
+            });
             layer.bindPopup(popupContent);
         }
     }).addTo(map);
 
     // create controls
-    createSequenceControls(dates);
+    createSequenceControls();
 
-    // initialize first date
-    updateSymbols(dates[0], geojsonLayer);
+    // initialize first month
+    updateSymbols(0, geojsonLayer);
 
-    // add slider events
+    // slider events
     var slider = document.querySelector(".range-slider");
 
     slider.addEventListener("input", function() {
-        updateSymbols(dates[this.value], geojsonLayer);
+        updateSymbols(Number(this.value), geojsonLayer);
     });
 
     document.querySelector("#forward").addEventListener("click", function() {
         var index = Number(slider.value);
-        index = (index + 1) % dates.length;
+        index = (index + 1) % 12;
         slider.value = index;
-        updateSymbols(dates[index], geojsonLayer);
+        updateSymbols(index, geojsonLayer);
     });
 
     document.querySelector("#reverse").addEventListener("click", function() {
         var index = Number(slider.value);
-        index = (index - 1 + dates.length) % dates.length;
+        index = (index - 1 + 12) % 12;
         slider.value = index;
-        updateSymbols(dates[index], geojsonLayer);
+        updateSymbols(index, geojsonLayer);
     });
 
-})
-.catch(error => console.log(error));
+});
 
-function updateSymbols(date, layerGroup) {
-
+// update circle sizes based on month
+function updateSymbols(monthIndex, layerGroup) {
+    var month = months[monthIndex];
     layerGroup.eachLayer(function(layer) {
-
-        if (layer.feature.properties.REPORT_DAT === date) {
-            layer.setStyle({ fillOpacity: 0.8 });
-        } else {
-            layer.setStyle({ fillOpacity: 0 });
-        }
-
+        var count = layer.feature.properties[month] || 0;
+        layer.setStyle({
+            radius: scaleRadius(count),
+            fillOpacity: count > 0 ? 0.8 : 0
+        });
     });
 }
-function processData(data) {
-    var dates = [];
 
-    data.features.forEach(function(feature) {
-        var date = feature.properties.REPORT_DAT;
-        if (dates.indexOf(date) === -1) {
-            dates.push(date);
-        }
-    });
-
-    dates.sort();
-    return dates;
+// proportional scaling function
+function scaleRadius(count) {
+    return 4 + Math.sqrt(count); // sqrt for better visual scaling
 }
 
-function createSequenceControls(dates) {
-
+// Sequence controls (month slider)
+function createSequenceControls() {
     var SequenceControl = L.Control.extend({
         options: { position: 'bottomleft' },
 
@@ -101,7 +90,8 @@ function createSequenceControls(dates) {
             container.innerHTML =
                 '<button class="step" id="reverse">Reverse</button>' +
                 '<input class="range-slider" type="range">' +
-                '<button class="step" id="forward">Forward</button>';
+                '<button class="step" id="forward">Forward</button>' +
+                '<div id="month-label"></div>';
 
             L.DomEvent.disableClickPropagation(container);
 
@@ -112,7 +102,14 @@ function createSequenceControls(dates) {
     map.addControl(new SequenceControl());
 
     var slider = document.querySelector(".range-slider");
-    slider.max = dates.length - 1;
+    slider.max = 11;
     slider.min = 0;
     slider.value = 0;
+    slider.step = 1;
+
+    // update label
+    slider.addEventListener("input", function() {
+        document.querySelector("#month-label").innerHTML = months[this.value];
+    });
+    document.querySelector("#month-label").innerHTML = months[0];
 }
